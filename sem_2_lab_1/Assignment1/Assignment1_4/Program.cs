bdd7dd81b9258c54b1bda2d44981f00b142b2c84
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 20:15:09 +0200

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_1/Assignment1/Assignment1_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string FilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_4\\Task4.txt";
+
+        string[] lines = File.ReadAllLines(FilePath);
+        string[] data = new string[3];
+
+        bool found = false;
+    }
+
+    static string[] Split(string line, string[] data)
+    {
+        
+    }
+}
