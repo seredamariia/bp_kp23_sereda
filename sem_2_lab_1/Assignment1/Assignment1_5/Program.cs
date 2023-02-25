e22f7688f53167ad0162a4d6dd834dbecdacb213
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 21:57:03 +0200

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_1/Assignment1/Assignment1_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string FilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_5\\Task5.txt";
+
+        string[] lines = File.ReadAllLines(FilePath);
+        string[,] WordsAndCounts = new string[1000, 2];
+
+        int index = 0;
+    }
+
+    static string[,] SplitAndCount(string lines, string[,] WordsAndCounts, int index)
+    {
+        return WordsAndCounts;
+    }
+}
