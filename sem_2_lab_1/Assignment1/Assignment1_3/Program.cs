93271a772d7c433c34a11d2dd6eced3b1e223ff9
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 19:42:34 +0200

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_1/Assignment1/Assignment1_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string InputFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_3\\input.txt";
+        string OutputFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_3\\output.txt";
+
+        string[] words = File.ReadAllLines(InputFilePath);
+
+        InsertionSort(words);
+
+        File.WriteAllLines(OutputFilePath, words);
+    }
+
+    static void InsertionSort(string[] array)
+    {
+        
+    }
+
+    static int StringsCompare(string s1, string s2)
+    {
+        
+    }
+}
