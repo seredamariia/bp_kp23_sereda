31153c6593d2e0e8c7e22672efb80ee549a17103
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 20:24:14 +0200

    Додано тестові сценарії

--- sem_2_lab_1/Assignment1/Assignment1_4/Program.cs
+++ sem_2_lab_1/Assignment1/Assignment1_4/Program.cs
@@ -8,10 +8,37 @@ class Program
     {
         string FilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_4\\Task4.txt";
 
+        /*test cases:
+            case 1:
+                Task4.txt: Lana,Beard,67
+                           Fleur,Salazar,52
+                           Gertrude,Caldwell,30
+            case 2:
+                Task4.txt: Keaton,Bernard,71
+                           Sana,Mueller,64
+                           Findlay,Cervantes,92
+            case 3:
+                Task4.txt: Woody,Bishop,33
+                           Alys,Burns,44
+                           Will,Gordon,55
+         */
+
         string[] lines = File.ReadAllLines(FilePath);
         string[] data = new string[3];
 
         bool found = false;
+
+        /*test cases:
+            case 1:
+                Fleur Salazar 52
+                Gertrude Caldwell 30
+            case 2:
+                There are no student with the score that is less than 60 points
+            case 3:
+                Woody Bishop 33
+                Alys Burns 44
+                Will Gordon 55
+         */
     }
 
     static string[] Split(string line, string[] data)
