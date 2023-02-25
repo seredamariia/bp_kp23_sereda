aa09c3298cb4b6720a4e72f5bda40f24ca9d4376
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 19:48:32 +0200

    Додано тестові сценарії

--- sem_2_lab_1/Assignment1/Assignment1_3/Program.cs
+++ sem_2_lab_1/Assignment1/Assignment1_3/Program.cs
@@ -7,11 +7,41 @@ class Program
         string InputFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_3\\input.txt";
         string OutputFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_3\\output.txt";
 
+        /*test cases:
+            case 1:
+                input.txt: programming
+                           basics
+                           office
+            case 2:
+                input.txt: catfish
+                           cat
+                           adorable
+            case 3:
+                input.txt: mainstream
+                           organisation
+                           queen
+         */
+
         string[] words = File.ReadAllLines(InputFilePath);
 
         InsertionSort(words);
 
         File.WriteAllLines(OutputFilePath, words);
+
+        /*test cases:
+            case 1:
+                input.txt: basics
+                           office
+                           programming
+            case 2:
+                input.txt: adorable
+                           cat
+                           catfish
+            case 3:
+                input.txt: mainstream
+                           organisation
+                           queen
+         */
     }
 
     static void InsertionSort(string[] array)
