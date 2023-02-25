5307763d4c37573c028e699a7439cafb6f68d65b
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 23:51:58 +0200

    Додано повну реалізацію програми

--- sem_2_lab_1/Assignment1/Assignment1_6/Program.cs
+++ sem_2_lab_1/Assignment1/Assignment1_6/Program.cs
@@ -31,25 +31,56 @@ class Program
 
         using (BinaryWriter writer = new BinaryWriter(File.Open(BinaryFilePath, FileMode.Create)))
         {
-            
+            foreach (string line in lines)
+            {
+                writer.Write(line);
+            }
         }
 
         using (BinaryWriter writer = new BinaryWriter(File.Open(HighScoresFilePath, FileMode.Create)))
         {
             using (BinaryReader reader = new BinaryReader(File.Open(BinaryFilePath, FileMode.Open)))
             {
-                
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Split(reader.ReadString(), data);
+
+                    if (Int32.Parse(data[2]) >= 95)
+                    {
+                        HighScoresRecords[HighScoresCount, 0] = data[0];
+                        HighScoresRecords[HighScoresCount, 1] = data[1];
+                        HighScoresRecords[HighScoresCount, 2] = data[2];
+                        HighScoresCount++;
+                    }
+                }
+            }
+
+            writer.Write("Number of students with high scores: " + HighScoresCount);
+
+            for (int i = 0; i < HighScoresCount; i++)
+            {
+                writer.Write(HighScoresRecords[i, 0] + " " + HighScoresRecords[i, 1] + " " + HighScoresRecords[i, 2]);
             }
         }
 
         using (BinaryReader reader = new BinaryReader(File.Open(HighScoresFilePath, FileMode.Open)))
         {
-            
+            if(HighScoresCount != 0)
+            {
+                for (int i = 0; i <= HighScoresCount; i++)
+                {
+                    Console.WriteLine(reader.ReadString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no students with the score greater than 95 points");
+            }
         }
 
         /*test cases:
             case 1:
-                There are no students with the score  greater than 95 points
+                There are no students with the score greater than 95 points
             case 2:
                 Number of students with high scores: 1
                 Eden Nunez 96
