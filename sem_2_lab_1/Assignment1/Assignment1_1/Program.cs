cf90f84442eb6030e9a11cd09b69ccb23c0c6e14
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 18:30:31 +0200

    Додано тестові сценарії

--- sem_2_lab_1/Assignment1/Assignment1_1/Program.cs
+++ sem_2_lab_1/Assignment1/Assignment1_1/Program.cs
@@ -9,6 +9,31 @@ class Program
     {
         string FilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_1\\Task1.txt";
         string[] lines = { };
+
+        /*test cases:
+            case 1: Write a program to create a text file that contains 2 lines.
+                    The content of the file should be displayed on the screen.
+            case 2: Content of the file.
+            case 3: Task 1
+                    Task 2
+                    Task 3
+                    Task 4
+                    Task 5
+                    Task 6
+        */
+
         string[] FileContent = File.ReadAllLines(FilePath);
+
+        /*test cases:
+            case 1: Write a program to create a text file that contains 2 lines.
+                    The content of the file should be displayed on the screen.
+            case 2: Content of the file.
+            case 3: Task 1
+                    Task 2
+                    Task 3
+                    Task 4
+                    Task 5
+                    Task 6
+        */
     }
 }
