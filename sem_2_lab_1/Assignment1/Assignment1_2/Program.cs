27a3f47d4e8bc776dab9f689c6ffb69b27625b1b
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-02-25 18:52:36 +0200

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_1/Assignment1/Assignment1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string NumbersFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_2\\numbers.txt";
+        string MaxFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_1\\Assignment1\\Assignment1_2\\max.txt";
+
+        int max = 0;
+
+        int[] numbers = new int[15];
+
+        Random rand = new Random();
+
+        using (StreamWriter sw = File.CreateText(NumbersFilePath))
+        {
+        }
+
+        using (StreamWriter sw = File.CreateText(MaxFilePath))
+        {
+        }
+    }
+}
