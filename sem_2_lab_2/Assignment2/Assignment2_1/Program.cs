dab39ec66454d651563b7b0eebf3209e0258d15e
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-03 19:14:21 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_2/Assignment2/Assignment2_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Employee
+{
+    private int id;
+    private string surname;
+    private decimal salary;
+    private decimal withheld;
+    private decimal issued;
+
+    public Employee(int id, string surname, decimal salary, decimal withheld)
+    {
+        this.id = id;
+        this.surname = surname;
+        this.salary = salary;
+        this.withheld = withheld;
+    }
+
+    public decimal GetSalary()
+    {
+        return salary;
+    }
+
+    public decimal GetWithheld()
+    {
+        return withheld;
+    }
+
+    public decimal Issued()
+    {
+        return issued;
+    }
+
+    public override string ToString()
+    {
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
