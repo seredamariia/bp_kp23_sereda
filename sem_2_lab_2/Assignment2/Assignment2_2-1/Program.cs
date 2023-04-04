9db86f154db36ac96b2f040358425d6672fd0f16
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-04 17:52:21 +0300

    Додано тестові сценарії

--- sem_2_lab_2/Assignment2/Assignment2_2-1/Program.cs
+++ sem_2_lab_2/Assignment2/Assignment2_2-1/Program.cs
@@ -63,6 +63,18 @@ class Program
 
         Console.WriteLine("\nEnter employees data (FORMAT OF THE DATA: SURNAME SALARY WITHHELD):");
 
+        /*test cases:
+            case 1: n = 0
+            case 2: n = 1
+                    Employees data:
+                                    Melnyk 10000 1000
+            case 3: n = 3
+                    Employees data:
+                                    Kubrak 15000 500
+                                    Malyshev 12500 750
+                                    Kochetkova 20000 0
+         */
+
         for (int i = 0; i < employees.Length; i++)
         {
             string[] input = Console.ReadLine().Split();
@@ -75,5 +87,21 @@ class Program
 
         Payroll payroll = new Payroll(employees);
         payroll.DisplayPayroll();
+
+        /*test cases:
+            case 1: #  Surname        Salary   Withheld     Issued
+
+                       Total            0,00       0,00       0,00
+            case 2: #  Surname        Salary   Withheld     Issued
+                    1  Melnyk      10 000,00   1 000,00   9 000,00
+
+                       Total       10 000,00   1 000,00   9 000,00
+            case 3: #  Surname        Salary   Withheld     Issued
+                    1  Kubrak      15 000,00     500,00  14 500,00
+                    2  Malyshev    12 500,00     750,00  11 750,00
+                    3  Kochetkova  20 000,00       0,00  20 000,00
+
+                       Total       47 500,00   1 250,00  46 250,00
+         */
     }
 }
