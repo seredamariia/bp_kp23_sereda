26707d688ede4b548704e8c9c4b7067b34e601bb
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-05 19:37:28 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_2/Assignment2/Assignment2_2-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+abstract class Vessel
+{
+    public abstract void PrepareToMoving();
+    public abstract void Move();
+}
+
+class SailingVessel : Vessel
+{
+    public override void PrepareToMoving()
+    {
+        
+    }
+
+    public override void Move()
+    {
+        
+    }
+}
+
+class Submarine : Vessel
+{
+    public override void PrepareToMoving()
+    {
+        
+    }
+
+    public override void Move()
+    {
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
