a650b4713f3e4a7e9847d2be6e6223c124b79302
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-05 20:06:38 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_2/Assignment2/Assignment2_2-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+interface IVessel
+{
+    
+}
+
+class SailingVessel : IVessel
+{
+    public void PrepareToMoving()
+    {
+        
+    }
+
+    public void Move()
+    {
+        
+    }
+}
+
+class Submarine : IVessel
+{
+    public void PrepareToMoving()
+    {
+        
+    }
+
+    public void Move()
+    {
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
