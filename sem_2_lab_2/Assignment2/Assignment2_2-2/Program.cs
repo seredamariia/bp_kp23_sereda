502a69d03e36d215053a4ccd2346f03f1f5511c9
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-05 19:51:06 +0300

    Додано повну реалізацію програми

--- sem_2_lab_2/Assignment2/Assignment2_2-2/Program.cs
+++ sem_2_lab_2/Assignment2/Assignment2_2-2/Program.cs
@@ -10,12 +10,12 @@ class SailingVessel : Vessel
 {
     public override void PrepareToMoving()
     {
-        
+        Console.WriteLine("Preparing the sails...");
     }
 
     public override void Move()
     {
-        
+        Console.WriteLine("Sailing away!");
     }
 }
 
@@ -23,12 +23,12 @@ class Submarine : Vessel
 {
     public override void PrepareToMoving()
     {
-        
+        Console.WriteLine("Diving underwater...");
     }
 
     public override void Move()
     {
-        
+        Console.WriteLine("Submerged and moving forward!");
     }
 }
 
@@ -36,6 +36,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Vessel vessel1 = new SailingVessel();
+        Vessel vessel2 = new Submarine();
+
+        vessel1.PrepareToMoving();
+        vessel1.Move();
+
+        Console.WriteLine();
+
+        vessel2.PrepareToMoving();
+        vessel2.Move();
     }
 }
