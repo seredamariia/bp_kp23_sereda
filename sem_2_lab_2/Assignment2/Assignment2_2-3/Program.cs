b0cc4d8852ddab6a6de3b50b5c2f5decdb286f1b
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-05 20:10:30 +0300

    Додано повну реалізацію програми

--- sem_2_lab_2/Assignment2/Assignment2_2-3/Program.cs
+++ sem_2_lab_2/Assignment2/Assignment2_2-3/Program.cs
@@ -2,19 +2,20 @@
 
 interface IVessel
 {
-    
+    void PrepareToMoving();
+    void Move();
 }
 
 class SailingVessel : IVessel
 {
     public void PrepareToMoving()
     {
-        
+        Console.WriteLine("Preparing the sails for movement...");
     }
 
     public void Move()
     {
-        
+        Console.WriteLine("Sailing away!");
     }
 }
 
@@ -22,12 +23,12 @@ class Submarine : IVessel
 {
     public void PrepareToMoving()
     {
-        
+        Console.WriteLine("Diving underwater...");
     }
 
     public void Move()
     {
-        
+        Console.WriteLine("Submerged and moving forward!");
     }
 }
 
@@ -35,6 +36,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        SailingVessel sailingVessel = new SailingVessel();
+        Submarine submarine = new Submarine();
+
+        sailingVessel.PrepareToMoving();
+        sailingVessel.Move();
+
+        Console.WriteLine();
+
+        submarine.PrepareToMoving();
+        submarine.Move();
     }
 }
