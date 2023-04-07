81c7fe4873da7ec9478636767153f9267d3aaebf
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-07 14:10:03 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_2/Assignment2/Assignment2_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Intrinsics;
+
+class Vector
+{
+    private int[] vector;
+
+    public Vector(int length)
+    {
+        
+    }
+
+    public void ReadElementsFromConsole()
+    {
+        
+    }
+
+    public int SumOfNegatives()
+    {
+        return sum;
+    }
+
+    public int SumOfNegatives(Vector otherVector)
+    {
+        return sum;
+    }
+
+    public int ProductOfEvenIndexes()
+    {
+        return product;
+    }
+
+    public int ProductOfEvenIndexes(Vector otherVector)
+    {
+        return prod;
+    }
+
+    public int NumberOfZeros()
+    {
+        return count;
+    }
+
+    public int NumberOfZeros(Vector otherVector)
+    {
+        return count;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
