25643fcacfe833a32bc975a1c4d3033e30932f64
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-04-12 16:28:30 +0300

    Added third homework from 19.02.2023

---
+++ c#_files/Homework3/Homework3/Homework3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\c#_files\\Homework3\\Homework3\\input.txt";
+        string outputFilePath = "C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\c#_files\\Homework3\\Homework3\\output.txt";
+
+        string[] lines = File.ReadAllLines(inputFilePath);
+
+        int emptyLines = RemoveIdenticalLines(lines);
+
+        Array.Resize(ref lines, lines.Length - emptyLines);
+
+        File.WriteAllLines(outputFilePath, lines);
+    }
+
+    static int RemoveIdenticalLines(string[] lines)
+    {
+        int i = 0, emptyLines = 0;
+
+        while (i < lines.Length - emptyLines)
+        {
+            int j = i + 1;
+            while (j < lines.Length - emptyLines)
+            {
+                if (lines[i] == lines[j])
+                {
+                    int k = j;
+                    while (k < lines.Length - emptyLines - 1)
+                    {
+                        lines[k] = lines[k + 1];
+                        k++;
+                    }
+                    emptyLines++;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            i++;
+        }
+
+        return emptyLines;
+    }
+}
