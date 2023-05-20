b8db3333ac3491c611eac66ff6fa410089b98e9c
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-05-20 23:25:41 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_3/Assignment3/Assignment3_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Deque<Item> : IIterator<Item>
+{
+    private class Node<T>
+    {
+        
+    }
+
+    public Deque()
+    {
+        
+    }
+
+    public bool IsEmpty()
+    {
+        
+    }
+
+    public int Size()
+    {
+        
+    }
+
+    public void AddFirst(Item item)
+    {
+        
+    }
+
+    public void AddLast(Item item)
+    {
+        
+    }
+
+    public Item RemoveFirst()
+    {
+        
+    }
+
+    public Item RemoveLast()
+    {
+        
+    }
+
+    public IIterator<Item> Iterator()
+    {
+        
+    }
+
+    public bool HasNext;
+
+    public Item MoveNext()
+    {
+        
+    }
+}
+
+public interface IIterator<T>
+{
+    bool HasNext { get; }
+    T MoveNext();
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+
+}
