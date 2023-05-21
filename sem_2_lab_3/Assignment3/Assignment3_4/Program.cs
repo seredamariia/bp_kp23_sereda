1abd35fcb3b63d9993e1ec37c1cd37acc015760a
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-05-21 21:27:05 +0300

    Додано юнітне тестування

--- sem_2_lab_3/Assignment3/Assignment3_4/Program.cs
+++ sem_2_lab_3/Assignment3/Assignment3_4/Program.cs
@@ -60,6 +60,90 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+        RandomizedQueue<int> test_queue = new RandomizedQueue<int>();
+        RandomizedQueueTests.TestConstructor();
+        RandomizedQueueTests.TestEnqueue(test_queue);
+        RandomizedQueueTests.TestIterator(test_queue);
+    }
+}
+
+public class RandomizedQueueTests
+{
+    public static bool TestConstructor()
+    {
+        RandomizedQueue<int> test_queue = new RandomizedQueue<int>();
+
+        int expected1 = 0;
+        int actual1 = test_queue.Size();
+
+        bool expected2 = true;
+        bool actual2 = test_queue.IsEmpty();
+
+        if (actual1 != expected1)
+        {
+            Console.WriteLine("TestConstructor: case1 FAILED");
+            return false;
+        }
+        if (actual2 != expected2)
+        {
+            Console.WriteLine("TestConstructor: case2 FAILED");
+            return false;
+        }
+
+        Console.WriteLine("TestConstructor: all cases PASSED");
+        return true;
+    }
+
+    public static bool TestEnqueue(RandomizedQueue<int> test_queue)
+    {
+        test_queue.Enqueue(1);
+        int expected1 = 1;
+        int actual1 = test_queue.Size();
+
+        test_queue.Enqueue(2);
+        int expected2 = 2;
+        int actual2 = test_queue.Size();
+
+        test_queue.Enqueue(3);
+        int expected3 = 3;
+        int actual3 = test_queue.Size();
+
+        if (actual1 != expected1)
+        {
+            Console.WriteLine("TestEnqueue: case1 FAILED");
+            return false;
+        }
+        if (actual2 != expected2)
+        {
+            Console.WriteLine("TestEnqueue: case2 FAILED");
+            return false;
+        }
+        if (actual3 != expected3)
+        {
+            Console.WriteLine("TestEnqueue: case3 FAILED");
+            return false;
+        }
+
+        Console.WriteLine("TestEnqueue: all cases PASSED");
+        return true;
+    }
+
+    public static void TestIterator(RandomizedQueue<int> test_queue)
+    {
+        Console.WriteLine("Testing Iterator()...");
+
+        test_queue.Enqueue(10);
+        test_queue.Enqueue(20);
+        test_queue.Enqueue(30);
+
+        Console.Write("Items in the queue: ");
+
+        IIterator<int> iterator = test_queue.Iterator();
+        while (iterator.HasNext)
+        {
+            Console.Write(iterator.MoveNext() + " ");
+        }
+
+        Console.WriteLine();
     }
 }
