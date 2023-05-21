e36cd5c71f841e67d49e2a316b84a91c4655f015
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-05-21 21:05:15 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_3/Assignment3/Assignment3_4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public interface IIterator<T>
+{
+    bool HasNext { get; }
+    T MoveNext();
+}
+
+public class RandomizedQueue<Item> : IIterator<Item>
+{
+    private class Node
+    {
+        
+    }
+
+    public RandomizedQueue()
+    {
+        
+    }
+
+    public bool IsEmpty()
+    {
+        
+    }
+
+    public int Size()
+    {
+        
+    }
+
+    public void Enqueue(Item item)
+    {
+        
+    }
+
+    public Item Dequeue()
+    {
+        
+    }
+
+    public Item Sample()
+    {
+        
+    }
+
+    public IIterator<Item> Iterator()
+    {
+        
+    }
+
+    public bool HasNext { get {  } }
+
+    public Item MoveNext()
+    {
+        
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
