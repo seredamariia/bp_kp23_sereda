096626ad207088eb5b75310830470dddfe9a328c
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-06-08 01:16:47 +0300

    Додано прототип програми на рівні заглушок

---
+++ sem_2_lab_4/Assignment4/Assignment4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class HashTable<KItem, VItem>
+{
+
+    public HashTable()
+    { 
+        
+    }
+
+    public HashTable(int intialCapacity)
+    { 
+         
+    }
+
+    public void Add(KItem key, VItem value)
+    {
+        
+    }
+
+    public void Remove(KItem key)
+    {
+        
+    }
+
+    public VItem Get(KItem key)
+    {
+        
+    }
+
+    public bool Contains(VItem key)
+    {
+        
+    }
+
+    public void clear()
+    {
+        
+    }
+
+    public int size()
+    {
+        
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
