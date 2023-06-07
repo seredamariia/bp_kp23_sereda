fd1764a7f77c2e9505959622ec021a1ae99c933b
seredamariia <114071332+seredamariia@users.noreply.github.com>
2023-06-08 01:35:10 +0300

    Додано повну реалізацію програми

--- sem_2_lab_4/Assignment4/Assignment4/Program.cs
+++ sem_2_lab_4/Assignment4/Assignment4/Program.cs
@@ -1,55 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 public class HashTable<KItem, VItem>
 {
+    private const int DefaultCapacity = 51;
+    private LinkedList<KeyValuePair<KItem, VItem>>[] buckets;
 
     public HashTable()
-    { 
-        
+    {
+        buckets = new LinkedList<KeyValuePair<KItem, VItem>>[DefaultCapacity];
     }
 
-    public HashTable(int intialCapacity)
-    { 
-         
+    public HashTable(int initialCapacity)
+    {
+        buckets = new LinkedList<KeyValuePair<KItem, VItem>>[initialCapacity];
+    }
+
+    private int GetBucketIndex(KItem key)
+    {
+        int hashCode = Math.Abs(key.GetHashCode());
+        int bucketIndex = hashCode % buckets.Length;
+        return bucketIndex;
     }
 
     public void Add(KItem key, VItem value)
     {
-        
+        int bucketIndex = GetBucketIndex(key);
+
+        if (buckets[bucketIndex] == null)
+            buckets[bucketIndex] = new LinkedList<KeyValuePair<KItem, VItem>>();
+
+        foreach (var pair in buckets[bucketIndex])
+        {
+            if (pair.Key.Equals(key))
+            {
+                throw new ArgumentException("An element with the same key already exists.");
+            }
+        }
+
+        buckets[bucketIndex].AddLast(new KeyValuePair<KItem, VItem>(key, value));
     }
 
     public void Remove(KItem key)
     {
-        
+        int bucketIndex = GetBucketIndex(key);
+
+        if (buckets[bucketIndex] != null)
+        {
+            var node = buckets[bucketIndex].First;
+
+            while (node != null)
+            {
+                if (node.Value.Key.Equals(key))
+                {
+                    buckets[bucketIndex].Remove(node);
+                    return;
+                }
+
+                node = node.Next;
+            }
+        }
     }
 
     public VItem Get(KItem key)
     {
-        
+        int bucketIndex = GetBucketIndex(key);
+
+        if (buckets[bucketIndex] != null)
+        {
+            foreach (var pair in buckets[bucketIndex])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return pair.Value;
+                }
+            }
+        }
+
+        throw new KeyNotFoundException("The given key was not found in the hash table.");
     }
 
-    public bool Contains(VItem key)
+    public bool Contains(KItem key)
     {
-        
+        int bucketIndex = GetBucketIndex(key);
+
+        if (buckets[bucketIndex] != null)
+        {
+            foreach (var pair in buckets[bucketIndex])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
-    public void clear()
+    public void Clear()
     {
-        
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            buckets[i]?.Clear();
+        }
     }
 
-    public int size()
+    public int Size()
     {
-        
+        int count = 0;
+
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            count += buckets[i]?.Count ?? 0;
+        }
+
+        return count;
     }
 }
-
 
 public class Program
 {
     public static void Main()
     {
-        
+        HashTable<string, bool> dictionary = new HashTable<string, bool>();
+
+        // Read words from text file and add them to the dictionary
+        string[] words = File.ReadAllLines("C:\\Users\\Марія\\KPI\\1 курс\\OP\\bp_kp23_sereda\\sem_2_lab_4\\Assignment4\\Assignment4\\words.txt");
+        foreach (string word in words)
+        {
+            dictionary.Add(word, true);
+        }
+
+        while (true)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine().Trim();
+
+            if (dictionary.Contains(word))
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("WrongSpelling");
+            }
+        }
     }
 }
